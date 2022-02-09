a2136bb3e74d365ecfe08a057a0ce37c8656b403
Charlie Dowley <dowleycharlotte@hotmail.co.uk>
2022-02-09 16:55:26 +0000

    Remove Vendors

--- vendor/craftcms/cms/src/web/assets/pluginstore/postcss.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-var tailwindcss = require('tailwindcss');
-
-module.exports = {
-    plugins: [
-        tailwindcss(path.resolve(__dirname, './tailwind.config.js')),
-        require('autoprefixer')
-    ],
-}
