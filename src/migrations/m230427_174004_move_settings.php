45a33b928309335abec3b40a4a0d88bcfbc2dd4f
Charlie Dowley <dowleycharlotte@hotmail.co.uk>
2023-04-27 19:04:33 +0100

    Add Logs to test settings

--- src/migrations/m230427_174004_move_settings.php
+++ src/migrations/m230427_174004_move_settings.php
@@ -12,11 +12,11 @@ use craft\db\Migration;
  */
 class m230427_174004_move_settings extends Migration
 {
-    /**
-     * @inheritdoc
-     */
-    public function safeUp(): bool
-    {
+	/**
+	 * @inheritdoc
+	 */
+	public function safeUp(): bool
+	{
 		$plugin = Craft::$app->getPlugins()->getPlugin("websitedocumentation");
 		$savedSettings = $plugin->settings;
 
@@ -25,38 +25,48 @@ class m230427_174004_move_settings extends Migration
 
 			$settings = [];
 
+			Craft::info($savedSettings, "DowleyDeveloped");
+
 			foreach ($sites->getEditableSites() as $editableSite) {
 				$handle = $editableSite->handle;
 				$newSettings = [
-					($handle) => [
-						'logo' 				=> $savedSettings->logo[0],
-						'brandBgColor'		=> $savedSettings->brandBgColor,
-						'brandTextColor'	=> $savedSettings->brandTextColor,
-						'accentBgColor'		=> $savedSettings->accentBgColor,
-						'accentTextColor'	=> $savedSettings->accentTextColor,
-						'displayStyleGuide'	=> $savedSettings->displayStyleGuide,
-						'displayCmsGuide'	=> $savedSettings->displayCmsGuide,
-					]
+					$handle => [
+						"logo" => $savedSettings->logo[0],
+						"brandBgColor" => $savedSettings->brandBgColor,
+						"brandTextColor" => $savedSettings->brandTextColor,
+						"accentBgColor" => $savedSettings->accentBgColor,
+						"accentTextColor" => $savedSettings->accentTextColor,
+						"displayStyleGuide" =>
+							$savedSettings->displayStyleGuide,
+						"displayCmsGuide" => $savedSettings->displayCmsGuide,
+					],
 				];
 
 				$settings = $newSettings;
 			}
 
-			if (!Craft::$app->getPlugins()->savePluginSettings($plugin, $settings)) {
+			Craft::info($settings, "DowleyDeveloped");
+
+			if (
+				!Craft::$app
+					->getPlugins()
+					->savePluginSettings($plugin, $settings)
+			) {
 				Craft::$app
 					->getSession()
-					->setError(Craft::t("app", "Couldn't save plugin settings."));
+					->setError(
+						Craft::t("app", "Couldn't save plugin settings.")
+					);
 			}
-
 		}
-    }
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function safeDown(): bool
-    {
-        echo "m230427_174004_move_settings cannot be reverted.\n";
-        return false;
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function safeDown(): bool
+	{
+		echo "m230427_174004_move_settings cannot be reverted.\n";
+		return false;
+	}
 }
